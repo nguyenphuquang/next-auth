6317705d59268e809f826df1108396efc7178657
Quang Nguyen <nguyenphuquang@gmail.com>
2023-02-06 16:55:09 +0700

    First commit

---
+++ core/routes/signin.d.ts
@@ -0,0 +1,9 @@
+import { IncomingRequest, OutgoingResponse } from "..";
+import { InternalOptions } from "../../lib/types";
+/** Handle requests to /api/auth/signin */
+export default function signin(params: {
+    options: InternalOptions<"oauth" | "email">;
+    query: IncomingRequest["query"];
+    body: IncomingRequest["body"];
+    headers: IncomingRequest['headers'];
+}): Promise<OutgoingResponse>;
